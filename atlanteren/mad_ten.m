d9c7f1193b9a7c7faa5b6cd89812cd6f6ff4dda9
Birger Johansen <birger.johansen@spacecentre.no>
2014-06-06 08:36:13 +0200

    Initial commit

---
+++ atlanteren/mad_ten.m
@@ -0,0 +1,46 @@
+load mad_ten_data;
+Y = data(:, 1);
+M = data(:, 2);
+D = data(:, 3);
+h = data(:, 4);
+m = data(:, 5);
+s = zeros(size(m));
+lat = data(:, 12);
+lon = data(:, 13);
+
+orthomap(0, 0);
+plotm(lat, lon, '-');
+plotm(lat, lon, '.');
+
+[gclat, gclon] = track2(lat(1), lon(1), lat(end), lon(end));
+plotm(gclat, gclon, 'r');
+
+lat2 = lat;
+lat2(1) = [];
+lat2 = [lat2;lat2(end)];
+lon2 = lon;
+lon2(1) = [];
+lon2 = [lon2;lon2(end)];
+
+rng = distance(lat, lon, lat2, lon2)*60;
+
+t = datenum(Y, M, D, h, m, s);
+t2 = t;
+t2(1) = [];
+t2 = [t2;t2(end)];
+
+dt = (t2-t)*24
+speed = rng./dt
+figure;
+plot(t, speed, '.-');
+
+cs = [];
+
+for i = 1:length(rng);
+    if i > 12
+        cs = [cs;sum(rng(i-12:i))];
+    end
+end
+        
+figure;
+plot(t, cs, '.-');
---
+++ atlanteren/stm_ftl.m
@@ -0,0 +1,35 @@
+load stm_ftl_data;
+Y = data(:, 1);
+M = data(:, 2);
+D = data(:, 3);
+h = data(:, 4);
+m = data(:, 5);
+s = zeros(size(m));
+lat = data(:, 12);
+lon = data(:, 13);
+
+orthomap(0, -40);
+plotm(lat, lon, '-');
+plotm(lat, lon, '.');
+
+[gclat, gclon] = track2(lat(1), lon(1), lat(end), lon(end));
+plotm(gclat, gclon, 'r');
+
+lat2 = lat;
+lat2(1) = [];
+lat2 = [lat2;lat2(end)];
+lon2 = lon;
+lon2(1) = [];
+lon2 = [lon2;lon2(end)];
+
+rng = distance(lat, lon, lat2, lon2)*60;
+
+t = datenum(Y, M, D, h, m, s);
+t2 = t;
+t2(1) = [];
+t2 = [t2;t2(end)];
+
+dt = (t2-t)*24
+speed = rng./dt
+figure;
+plot(t, speed, '.-');
